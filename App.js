e342915d42e7a98b4e0d5280e1984c455b8c9902
Dekpo Yologaza <dekpo@me.com>
2020-12-21 20:17:02 +0100

    few modifications

--- App.js
+++ App.js
@@ -86,8 +86,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image:{
-    width:200,
-    height:200
+    width:300,
+    height:300
   },
   infos: {
     color: 'blue',
