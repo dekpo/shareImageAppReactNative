f69f77491199e8124de54b6f89f72f14f70f4f91
Dekpo Yologaza <dekpo@me.com>
2020-12-21 17:43:36 +0100

    Created a full app to launch and share an image from the library

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
 
 export default function App() {
+//utilisation du State pour stocker l'image reçue
+const [selectedImage, setSelectedImage] = React.useState(null);
+
+// obtenir l'autorisation d'accéder à la Galerie d'images du device
+// si c'est ok on retourne les infos
+let openImagePickerAsync = async () => {
+  console.log(Platform.OS,'OKok !');
+  let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  // si la permission est invalide on stoppe avec un return
+  if (permissionResult.granted === false){
+    alert("Permission to access camera and library is required !");
+    return;
+  }
+  let pickerResult = await ImagePicker.launchImageLibraryAsync();
+  console.log('pickerResult: ',pickerResult );
+ // si la sélection est annulée on stoppe tout avec un return
+  if (pickerResult.cancelled === true){
+    return
+  }
+  // sinon c'est bon on stocke l'adresse de l'image dans une propriété localUri
+  setSelectedImage( {localUri: pickerResult.uri} );
+}
+
+// methode pour partager l'image
+let openShareAsync = async () => {
+  if ( !(await Sharing.isAvailableAsync()) ) {
+    alert("Oups no sharing available on your platform...");
+    return
+  }
+  await Sharing.shareAsync( selectedImage.localUri );
+}
+
+// si selectedImage n'est pas null c'est qu'on a bien chargé une image
+// on peut créer un élément View à la volée avec la fameuse image dedans
+if (selectedImage !== null){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+      source={{uri: selectedImage.localUri }}
+      style={{width: 300, height:300, resizeMode: 'contain'}} />
+    
+      <TouchableOpacity
+      onPress={ openShareAsync }
+      style={{backgroundColor: 'blue',padding:10}}>
+        <Text style={{color: 'white'}}>SHARE YOUR IMAGE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      onPress={ () => { setSelectedImage(null) } }
+      style={{backgroundColor: 'blue',padding:10, marginTop:10}}>
+        <Text style={{color: 'white'}}>OR CANCEL</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+  return (
+    <View style={styles.container}>
+      <Image
+      source={ require ('./assets/dummy-image-portrait.jpg')}
+      style={ styles.image } 
+      />
+      <Text style={styles.infos}>Please Choose an image to share !!!</Text>
+      
+      <TouchableOpacity
+      onPress={ openImagePickerAsync }
+      style={{backgroundColor: 'blue',padding:10}}>
+        <Text style={{color: 'white'}}>PICK A PHOTO</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +85,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width:200,
+    height:200
+  },
+  infos: {
+    color: 'blue',
+    // fontFamily: 'sans-serif'
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
 });
